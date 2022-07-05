e4737740f28cbd1cfc5efb68693879c2de42f3b7
Samantha Leal <samantha.luchmann@gmail.com>
2022-07-05 19:10:41 -0300

    Feature: VehicleRepo created

--- spring03/src/main/java/com/br/meli/spring03/repositories/VehicleRepo.java
+++ spring03/src/main/java/com/br/meli/spring03/repositories/VehicleRepo.java
@@ -1,5 +1,25 @@
 package com.br.meli.spring03.repositories;
 
+import com.br.meli.spring03.models.Vehicle;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
 public class VehicleRepo {
+    private final String LINKFILE = "src/main/resources/data.json";
+
+    public List<Vehicle> getAllVehicle(){
+        ObjectMapper mapper = new ObjectMapper();
+        List<Vehicle> list = null;
+        try {
+            list = Arrays.asList(
+                    mapper.readValue(new File(LINKFILE), Vehicle[].class));
+        } catch (Exception ex) {
+
+        }
+        return list;
+    }
 
 }
